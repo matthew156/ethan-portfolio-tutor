3a946f9ad1c70f8e37c6b444da399ea4023e440e
Matt <matthewastro@gmail.com>
2022-03-11 07:09:56 -0500

    Fixed and added server to main.py

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+<<<<<<< Updated upstream
+=======
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+>>>>>>> Stashed changes
